001498c8b7d61c47a63f2566c9211f628b6bf4a1
idbmb <bmb.router@gmail.com>
2017-03-17 19:02:16 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # GimpLikePhotoshop
 How to quickly change interface gimp like photoshop
+
+To help a fellow deviant, I've put together a download that anyone with Gimp 2.8 on Linux can use to make their copy of Gimp look more like Adobe Photoshop and make people more comfortable.
+
+I've taken various bits of guides online and various tweaks.
+All you have to do is download the file, unzip it.
+Then in the file manager go to your home folder, show hidden files, delete or move your old .gimp-2.8 configuration folder out of the way, and replace with the new one.
+Then load Gimp.
+
+Please let me know if it works for you or if it fails.
+
+## Getting Started
+
+### Prerequisites
+
+__Disclaimer:__ _Gimp Like Photoshop supports Windows, but works best on OS X and Linux._
+
+* Gimp `2.8`
+* `git` should be installed
+
+### Basic Installation
+
+GimpLikePhotoshop is installed by running one of the following commands in your terminal. You can install this via the command-line with either `curl` or `wget`.
+
+#### via curl
+
+```shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/idbmb/GimpLikePhotoshop/master/tools/install.sh)"
+```
+
+#### via wget
+
+```shell
+sh -c "$(wget https://raw.githubusercontent.com/idbmb/GimpLikePhotoshop/master/tools/install.sh -O -)"
+```
+
+### Mac installation
+
+	# exit gimp first
+	cd $HOME/Library/Application\ Support/GIMP
+	mv 2.8 2.8.backup
+	git clone --depth=1 https://github.com/idbmb/GimpLikePhotoshop.git 2.8
+
+### Windows installation
+
+#### via powershell:
+
+	# exit gimp first
+	cd $Env:UserProfile
+	mv .gimp-2.8 .gimp-2.8.backup
+	git clone --depth=1 https://github.com/idbmb/GimpLikePhotoshop.git .gimp-2.8
+
+#### via cmd:
+
+	# exit gimp first
+	cd %USERPROFILE%
+	ren .gimp-2.8 .gimp-2.8.backup
+	git clone --depth=1 https://github.com/idbmb/GimpLikePhotoshop.git .gimp-2.8
---
+++ tools/install.sh
@@ -0,0 +1,66 @@
+# Resource: https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+
+main() {
+  # Use colors, but only if connected to a terminal, and that terminal
+  # supports them.
+  if which tput >/dev/null 2>&1; then
+      ncolors=$(tput colors)
+  fi
+  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+  else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+  fi
+  # Only enable exit-on-error after the non-critical colorization stuff,
+  # which may fail on systems lacking tput or terminfo
+  set -e
+
+  gimp_path=$(which gimp)
+  if [ $? -ne 0 ]; then
+    printf "${YELLOW}Gimp is not installed!${NORMAL} Please install Gimp first!\n"
+    exit 1
+  fi
+
+  version='2.8'
+  gimp_path_version_string=$($gimp_path --version)
+  if echo "$gimp_path_version_string" | grep -qvF "$version"; then
+    printf "${YELLOW}Gimp version is not installed!${NORMAL} Please install Gimp $version first!\n"
+    exit 1
+  fi
+
+  printf "${BLUE}Cloning Gimp Like Photoshop...${NORMAL}\n"
+  hash git >/dev/null 2>&1 || {
+    echo "Error: git is not installed"
+    exit 1
+  }
+
+  gimp_ps_directory="$HOME/.gimp-$version"
+
+  # Backup previous directory, if any
+  if [ -e "$gimp_ps_directory" ]; then
+    now=$(date +"%Y%m%d%H%M%S")
+    mv "$gimp_ps_directory" "$gimp_ps_directory.backup.$now"
+  fi
+
+  env git clone --depth=1 https://github.com/idbmb/GimpLikePhotoshop.git $gimp_ps_directory || {
+    printf "Error: git clone of Gimp Like Photoshop repo failed\n"
+    exit 1
+  }
+
+  printf "${GREEN}"
+  echo 'Gimp Like Photoshop successfully installed'
+  echo 'Enjoy!'
+  printf "${NORMAL}"
+}
+
+main
